b9e2302639c2054c25f6c5d1e636df45b324118c
likui628 <likui.it@gmail.com>
2022-06-05 14:45:07 +0800

    refactor: multi-module maven project

--- pet-clinic-web/src/main/java/spring/petclinic/PetClinicWebApplication.java
+++ pet-clinic-web/src/main/java/spring/petclinic/PetClinicWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringPetclinicApplication {
+public class PetClinicWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringPetclinicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicWebApplication.class, args);
+    }
 
 }
