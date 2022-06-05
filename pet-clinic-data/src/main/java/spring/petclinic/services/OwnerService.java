a804ca48863ea95f4d2f806ac36adf34591dcce6
likui628 <likui.it@gmail.com>
2022-06-05 17:06:27 +0800

    chore: adding interface for services

---
+++ pet-clinic-data/src/main/java/spring/petclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package spring.petclinic.services;
+
+import spring.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
