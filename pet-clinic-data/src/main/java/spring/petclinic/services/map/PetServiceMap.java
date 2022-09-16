65cd0048b9e9e634500b9a8012e3fc08dfbaeca3
Li Kui <90845831+likui628@users.noreply.github.com>
2022-09-16 22:18:11 +0800

    chore: adding bootstrap for data

--- pet-clinic-data/src/main/java/spring/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/spring/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package spring.petclinic.services.map;
 
 import spring.petclinic.model.Pet;
-import spring.petclinic.services.CrudService;
+import spring.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/spring/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/spring/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package spring.petclinic.services.map;
 
 import spring.petclinic.model.Vet;
-import spring.petclinic.services.CrudService;
+import spring.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
