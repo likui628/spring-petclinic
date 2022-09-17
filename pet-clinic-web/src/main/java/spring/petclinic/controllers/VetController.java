db002e2740d488b0ca3a907fdd1e668ffcc64985
Li Kui <90845831+likui628@users.noreply.github.com>
2022-09-17 10:24:43 +0800

    feat: list Vets

--- pet-clinic-web/src/main/java/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/spring/petclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package spring.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import spring.petclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        System.out.println(vetService.findAll());
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
