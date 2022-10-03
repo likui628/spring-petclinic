0460ff2130662543161486c7c51c8b7a6ff6cf17
Li Kui <90845831+likui628@users.noreply.github.com>
2022-10-03 21:39:51 +0800

    convert Entities to JPA Entities

--- pet-clinic-data/src/main/java/spring/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/spring/petclinic/model/Person.java
@@ -1,7 +1,20 @@
 package spring.petclinic.model;
 
+
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
+
+    @Column(name = "first_name")
+    @NotNull
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
