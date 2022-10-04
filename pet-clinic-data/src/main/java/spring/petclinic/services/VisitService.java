f83866edf316a0ee5e79338c01e77c170ffc4fae
Li Kui <90845831+likui628@users.noreply.github.com>
2022-10-04 14:49:39 +0800

    add VisitService & VisitServiceMap

---
+++ pet-clinic-data/src/main/java/spring/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package spring.petclinic.services;
+
+import spring.petclinic.model.Visit;
+
+public interface VisitService extends  CrudService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/spring/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,37 @@
+package spring.petclinic.services.map;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import spring.petclinic.model.Visit;
+import spring.petclinic.services.VisitService;
+
+import java.util.Set;
+
+@Service
+@Profile({"default", "map"})
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
